c49ad7ce77a2114e79d3e4e978738a9aa4be5677
“xiake <YOU@EXAMPLE.COM>
2020-06-27 20:01:46 +0800

    修改了报错 添加了test方法

--- web_project/HelloWorld.java
+++ web_project/HelloWorld.java
@@ -1,5 +1,8 @@
-public Class{
-  public void main(String args[]){
-    System.out.print("HelloWorld")
+public class HelloWorld{
+  public static void main(String args[]){
+    System.out.print("HelloWorld");
+  }
+  public void test(){
+    System.out.println("添加了测试方法");
   }
 }
